068ee717f98e8e6f3c6f73603b7b3e921181c485
並木裕介 <namikiyuusuke@namikiyuusukenoMacBook-Pro.local>
2023-02-06 17:44:02 +0900

    img_path pt2

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -14,8 +14,19 @@ class ImageController extends Controller
         // アップロードされたファイル名を取得
         $file_name = $request->file('img_path')->getClientOriginalName();
 
+        // 取得したファイル名で保存
+        $request->file('img_ぱth')->storeAs('public' . $dir, $file_name);
+
         // publicディレクトリに画像を保存
-        $request->file('img_path')->store('public/' . $dir);
+        // $request->file('img_path')->store('public/' . $dir);
+
+        // ファイル情報をDBに保存
+        $img_path = new Image();
+        $img_path->name = $file_name;
+        $img_path->path = 'storage/' . $dir . '/' . $file_name;
+        $img_path->save();
+
+        return redirect('/');
 
         return redirect('/');
     }
